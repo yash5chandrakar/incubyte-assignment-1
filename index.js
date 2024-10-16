a9806bb9cb8798d585c3cd3a525938577de217e2
yash5chandrakar <yash5chandrakar@gmail.com>
2024-10-16 18:26:27 +0530

    Handle number separation by lines

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 
 // let str = "1,2,3"
 const add = (str) => {
+    if(!str){
+        return 0;
+    }
+
     let result = 0;
+
+    // console.log(str)
+
+    str = str.replace(/(?:\r\n|\r|\n)/g, ",");
+
+    // console.log(str)
+
     
     str?.split(",")?.map((el)=>{
-        result+=parseInt(el)
+        result+=parseInt(el)||0;
     })
 
     return result
 }
 
-console.log(add("1,2,3"))
+console.log(add("1\n2,3"))
