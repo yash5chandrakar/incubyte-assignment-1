ec50b6c220aed9361f90f76ca173a5b7443a56c5
yash5chandrakar <yash5chandrakar@gmail.com>
2024-10-16 19:29:04 +0530

    Negative Number changes

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 // let str = "1,2,3"
 const add = (str) => {
-    if(!str){
+    if (!str) {
         return 0;
     }
 
     let result = 0;
     let defaultSeparator = ","
-    let includesDelimiter = str?.includes("//")
+    let includesDelimiter = str?.includes("//");
+    let negativeNumberArray = []
 
-    if(includesDelimiter){
+    if (includesDelimiter) {
         let strArray = str?.split('\n', 2);
         let newDelimiter = strArray[0]?.slice(2);
         defaultSeparator = newDelimiter
@@ -23,12 +24,19 @@ const add = (str) => {
 
     // console.log(str)
 
-    
-    str?.split(defaultSeparator)?.map((el)=>{
-        result+=parseInt(el)||0;
+
+    str?.split(defaultSeparator)?.map((el) => {
+        if(parseInt(el)<0){
+            negativeNumberArray.push(el)
+        }
+        result += parseInt(el) || 0;
     })
+
+    if(negativeNumberArray?.length>0){
+        throw new Error(`negative numbers not allowed : ${negativeNumberArray?.join(", ")}`);
+    }
 
     return result
 }
 
-console.log(add("//;\n1;2"))
+console.log(add("//;\n1;-2;-3;-5"))
